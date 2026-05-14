65fb1a6c172a7dfa72f0867d799e24d7b153e18b
YoursTrulyAD <AD.dev@hotmail.com>
2026-05-13 20:47:43 -0400

    Updated calculator

--- calculator.py
+++ calculator.py
@@ -21,4 +21,14 @@ rectangle_area = height * base
 square_area = height * height
 circle_area = 3.14 * (height ** 2)
 
-print(f"The area is {}")
+if user_input == 1:
+    print(f"Area of Triangle: {triangle_area}")
+elif user_input == 2:
+    print(f"Area of Rectangle: {rectangle_area}")
+elif user_input == 3:
+    print(f"Area of Square: {square_area}")
+elif user_input == 4:
+    print(f"Area of Circle: {circle_area}")
+elif user_input == 5:
+    print("Goodbye!")
+else:    print("Invalid input. Please select a number between 1 and 5.")
